b620ab605851175e56487f9ecf6f8d5453c82a1e
LTree0703 <108115115+LTree0703@users.noreply.github.com>
2024-07-08 16:34:02 +0800

    Remove experimental ppr

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        ppr: 'incremental', // Incremental value allows you to adopt PPR in specific routes
-    },
+    // experimental: {
+    //     ppr: 'incremental', // Incremental value allows you to adopt PPR in specific routes
+    // },
 };
 
 export default nextConfig;
